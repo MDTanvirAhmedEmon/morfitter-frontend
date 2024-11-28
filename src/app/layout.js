feeedacf43a20904fb9ce3ef1a83f7061dfe5c18
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2024-11-28 17:10:32 +0600

    done register page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,16 @@
-
+import { ConfigProvider } from "antd";
 import "./globals.css";
+import { mainTheme } from "@/theme/ant-theme";
 
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>
-        {children}
-      </body>
+      <ConfigProvider theme={mainTheme}>
+        <body>
+          {children}
+        </body>
+      </ConfigProvider>
     </html>
   );
 }
---
+++ src/theme/ant-theme.js
@@ -0,0 +1,15 @@
+export const mainTheme = {
+    token: {
+        // colorBgBase: "#023E8A",
+    },
+    components: {
+        "Input": {
+            "inputFontSize": 16,
+            "controlHeight": 40,
+            "colorBorder": "rgb(11,165,147)",
+            "hoverBorderColor": "rgb(11,165,147)",
+            "activeBorderColor": "rgb(11,165,147)"
+        }
+
+    },
+};
