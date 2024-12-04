975143e13e845054edc5bdc6e4df680278fb7973
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2024-12-04 10:29:31 +0600

    made profile responsive

--- src/app/(main-layout)/profile/page.js
+++ src/app/(main-layout)/profile/page.js
@@ -17,11 +17,11 @@ const Profile = () => {
     const profilePicUrl = profilePic ? URL.createObjectURL(profilePic) : null;
 
     return (
-        <section className="welcome py-20">
+        <section className=" py-10 md:py-20">
             <div className="container mx-auto flex">
-                <div className="details-info flex gap-5 p-5 min-h-[660px] w-full rounded-lg shadow-lg">
+                <div className="details-info flex flex-col lg:flex-row gap-5 p-5 min-h-[660px] w-full rounded-lg shadow-lg">
                     {/* Profile Section */}
-                    <div className="profile-details flex flex-col items-center gap-4 w-[25%]">
+                    <div className="profile-details flex flex-col items-center gap-4 w-full lg:w-[25%]">
 
                         <div className="relative mb-8 md:-mt-6">
                             <Image src={circle} className=" absolute  w-[300px]" alt="circle" height={0} width={0} />
@@ -81,79 +81,82 @@ const Profile = () => {
                     </div>
 
                     {/* Right Information Section */}
-                    <div className="right-information w-[75%] pt-5">
-                        <div className="user-details flex justify-between gap-5">
+                    <div className="right-information w-full lg:w-[75%] pt-5">
+                        <div className="user-details flex flex-col lg:flex-row lg:justify-between gap-5">
                             <div className="user">
                                 <div className="user-name text-4xl font-semibold">Emma Watson</div>
                                 <div className="mt-2 text-2xl">New York</div>
                             </div>
 
-                            <div className="following-follower grid grid-cols-3 gap-5">
+                            <div className="following-follower grid grid-cols-1 md:grid-cols-3 gap-5">
 
-                                <div className="item text-center px-14 py-4 rounded-xl bg-[#0ba5931a] border border-greenColor shadow-lg">
-                                    <div className="total text-3xl font-bold text-greenColor">50</div>
+                                <div className="item text-center px-14 py-1 md:py-4 rounded-xl bg-[#0ba5931a] border border-greenColor shadow-lg">
+                                    <div className="total text-xl md:text-3xl font-bold text-greenColor">50</div>
                                     <div className="title text-lg  text-greenColor capitalize">Followers</div>
                                 </div>
 
-                                <div className="item text-center px-14 py-4 bg-[#e2697121] border border-primary rounded-xl shadow-lg">
-                                    <div className="total text-3xl font-bold text-primary">30</div>
+                                <div className="item text-center px-14 py-1 md:py-4 bg-[#e2697121] border border-primary rounded-xl shadow-lg">
+                                    <div className="total text-xl md:text-3xl font-bold text-primary">30</div>
                                     <div className="title text-lg text-primary capitalize">Members</div>
                                 </div>
 
-                                <div className="item text-center px-14 py-4 border bg-[#e2697121] border-black rounded-xl shadow-lg">
-                                    <div className="total text-3xl font-bold text-black">£46</div>
+                                <div className="item text-center px-14 py-1 md:py-4 border bg-[#e2697121] border-black rounded-xl shadow-lg">
+                                    <div className="total text-xl md:text-3xl font-bold text-black">£46</div>
                                     <div className="title text-lg text-gray-900 capitalize">Revenue</div>
                                 </div>
 
                             </div>
                         </div>
 
-                        <div className="qualification-details mt-6">
-                            <div className="qualification flex justify-between items-center border py-2 px-4 rounded-md mb-4">
-                                <div className=" flex  gap-3 pr-[300px]">
-                                    <h2 className="title text-2xl font-medium text-[#535353]">Qualification</h2>
-                                    <div>
-                                        <p className=" text-gray-400 mt-1">(NOTES: Person needs to be abel tot add multiple refer to https://www.yourpersonaltraininguk.co.uk/trainers/barbara-veloso)</p>
-                                    </div>
+                        <div className=" mt-6">
+                            <div className="w-full flex justify-between items-center border py-4 px-6 rounded-md mb-4">
+                                <p className="text-lg  md:text-2xl block md:hidden font-medium mr-3">Qualification</p>
+                                <div className="flex flex-wrap gap-3 md:pr-0 xl:pr-[200px]">
 
+                                    <p className=" hidden md:block text-[#535353] w-full md:w-auto text-sm md:text-base">
+                                        <span className="text-lg md:text-2xl font-medium mr-3">Qualification</span>
+                                        (NOTES: Person needs to be abel tot add multiple refer to https://www.yourpersonaltraininguk.co.uk/trainers/barbara-veloso)
+                                    </p>
                                 </div>
-
-                                <button className="add-btn text-white bg-secondary px-6 py-[2px] rounded-full">Add</button>
+                                <button className="add-btn text-white bg-secondary px-3 md:px-6 py-0 md:py-2 rounded-full">
+                                    Add
+                                </button>
                             </div>
 
-                            <div className="qualification flex justify-between items-center border py-2 px-4 rounded-md mb-4">
-                                <div className=" flex  gap-3 pr-[300px]">
-                                    <h2 className="title text-2xl font-medium text-[#535353]">Specialisms</h2>
-                                </div>
 
-                                <button className="add-btn text-white bg-secondary px-6 py-[2px] rounded-full">Add</button>
+
+                            <div className="qualification flex  justify-between items-center border py-4 px-6 rounded-md mb-4">
+                                <div className="flex  gap-3 md:pr-8">
+                                    <h2 className="title text-lg md:text-2xl font-medium text-[#535353]">Specialisms</h2>
+                                </div>
+                                <button className="add-btn text-white bg-secondary px-3 md:px-6 py-0 md:py-2 rounded-full ">Add</button>
                             </div>
 
-                            <div className="qualification flex justify-between items-center py-2 px-4 rounded-md mb-4">
-                                <div className=" flex  gap-3 pr-[300px]">
-                                    <h2 className="title text-2xl font-medium text-[#535353]">Customer testimonials</h2>
+
+                            <div className="qualification flex  justify-between items-center py-4 px-6 rounded-md mb-4">
+                                <div className="flex  gap-3 md:pr-8">
+                                    <h2 className="title text-lg md:text-2xl font-medium text-[#535353]">Customer Testimonials</h2>
                                 </div>
-
-                                <button className="add-btn text-white bg-secondary px-6 py-[2px] rounded-full">Add</button>
+                                <button className="add-btn text-white bg-secondary px-3 md:px-6 py-0 md:py-2 rounded-full ">Add</button>
                             </div>
-
                         </div>
 
+
                         {/* Blogging Section */}
-                        <div className=" flex gap-5 w-full mt-4">
+                        <div className=" flex flex-col md:flex-row gap-5 w-full mt-4">
                             <div className="qualification flex justify-between items-center w-full mb-4 shadow-lg py-4 px-3 rounded-lg">
-                                <div className=" text-gray-500 text-xl font-bold">Blog</div>
-                                <button className="add-btn text-white bg-[#0ba5931a] border border-greenColor px-4 py-[14px] rounded-lg "><FaPlus className=" text-greenColor" /></button>
+                                <div className=" text-gray-500 text-lg md:text-xl font-bold">Blog</div>
+                                <button className="add-btn text-white bg-[#0ba5931a] border border-greenColor px-2 md:px-4 py-1 md:py-[14px] rounded-lg "><FaPlus className=" text-greenColor" /></button>
                             </div>
 
                             <div className="qualification flex justify-between items-center w-full mb-4 shadow-lg py-4 px-3 rounded-lg">
-                                <div className=" text-gray-500 text-xl font-bold">Video</div>
-                                <button className="add-btn text-white bg-[#0ba5931a] border border-greenColor px-4 py-[14px] rounded-lg "><FaPlus className=" text-greenColor" /></button>
+                                <div className=" text-gray-500 text-lg md:text-xl font-bold">Video</div>
+                                <button className="add-btn text-white bg-[#0ba5931a] border border-greenColor px-2 md:px-4 py-1 md:py-[14px] rounded-lg "><FaPlus className=" text-greenColor" /></button>
                             </div>
 
                             <div className="qualification flex justify-between items-center w-full mb-4 shadow-lg py-4 px-3 rounded-lg">
-                                <div className=" text-gray-500 text-xl font-bold">Images</div>
-                                <button className="add-btn text-white bg-[#0ba5931a] border border-greenColor px-4 py-[14px] rounded-lg "><FaPlus className=" text-greenColor" /></button>
+                                <div className=" text-gray-500 text-lg md:text-xl font-bold">Images</div>
+                                <button className="add-btn text-white bg-[#0ba5931a] border border-greenColor px-2 md:px-4 py-1 md:py-[14px] rounded-lg "><FaPlus className=" text-greenColor" /></button>
                             </div>
                         </div>
 
