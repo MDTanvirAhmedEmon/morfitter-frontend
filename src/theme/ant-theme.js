b76880a4032a79d247b11094c4102abcba015cbd
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2024-12-22 13:06:26 +0600

    design done creating session page

--- src/theme/ant-theme.js
+++ src/theme/ant-theme.js
@@ -9,7 +9,23 @@ export const mainTheme = {
             "colorBorder": "rgb(11,165,147)",
             "hoverBorderColor": "rgb(11,165,147)",
             "activeBorderColor": "rgb(11,165,147)"
+        },
+        "DatePicker": {
+            "activeBorderColor": "rgb(11,165,147)",
+            "hoverBorderColor": "rgb(11,165,147)",
+            "colorBorder": "rgb(11,165,147)",
+            "cellRangeBorderColor": "rgb(226,105,114)",
+            "colorPrimary": "rgb(226,105,114)",
+            "inputFontSize": 16,
+            "controlHeight": 40
+        },
+        "Select": {
+            "activeBorderColor": "rgb(11,165,147)",
+            "hoverBorderColor": "rgb(11,165,147)",
+            "colorPrimary": "rgb(11,165,147)",
+            "controlHeight": 40,
+            "fontSize": 16,
+            "colorBorder": "rgb(11,165,147)"
         }
-
     },
 };
