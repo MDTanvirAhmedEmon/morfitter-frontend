335d6bf653659c3b6389d68ae0be751dff815174
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-01-02 10:42:48 +0600

    add filter in content

--- src/app/(main-layout)/content/page.js
+++ src/app/(main-layout)/content/page.js
@@ -1,8 +1,10 @@
+"use client"
 import Image from 'next/image';
 import gymbg from '../../../assets/content/gym1.png';
 import gymbg2 from '../../../assets/content/gym2.png';
 import fitnessTeam from '../../../assets/content/fitnessTeam.png';
 import SingleBlog from '@/components/Content/SingleBlog';
+import { Select } from 'antd';
 
 const Content = () => {
     return (
@@ -18,7 +20,36 @@ const Content = () => {
                     alt="person"
                 />
             </div>
-            <div className=' py-12 md:py-20'>
+            <div className=' grid md:grid-cols-3 gap-10 mx-5 py-8 md:py-16'>
+                <Select className=' ' placeholder={<p className=" text-lg">Trainer Or Member</p>}>
+                    <Select.Option value="Live">Trainer</Select.Option>
+                    <Select.Option value="Recorded">Member</Select.Option>
+                </Select>
+                <Select className=" -mt-3 md:-mt-0" placeholder={<p className="text-lg">specialism</p>}>
+                    <Select.Option value="Ab Workouts">Ab Workouts</Select.Option>
+                    <Select.Option value="Anaerobic exercise">Anaerobic exercise</Select.Option>
+                    <Select.Option value="Boxercise">Boxercise</Select.Option>
+                    <Select.Option value="Calisthenics">Calisthenics</Select.Option>
+                    <Select.Option value="Circuits">Circuits</Select.Option>
+                    <Select.Option value="Core Strength">Core Strength</Select.Option>
+                    <Select.Option value="Fat Burners">Fat Burners</Select.Option>
+                    <Select.Option value="Flexibility & Mobility">Flexibility & Mobility</Select.Option>
+                    <Select.Option value="HIIT">HIIT</Select.Option>
+                    <Select.Option value="Nutritionist">Nutritionist</Select.Option>
+                    <Select.Option value="Pilates">Pilates</Select.Option>
+                    <Select.Option value="Senior Fitness">Senior Fitness</Select.Option>
+                    <Select.Option value="Spin">Spin</Select.Option>
+                    <Select.Option value="Yoga">Yoga</Select.Option>
+                    <Select.Option value="Weights">Weights</Select.Option>
+                    <Select.Option value="Zumba">Zumba</Select.Option>
+                    <Select.Option value="Other">Other</Select.Option>
+                </Select>
+                <Select className=' -mt-3 md:-mt-0' placeholder={<p className=" text-lg">Format</p>}>
+                    <Select.Option value="Onlien">Onlien</Select.Option>
+                    <Select.Option value="In-Person">In-Person</Select.Option>
+                </Select>
+            </div>
+            <div className=' pb-12 md:pb-20'>
                 <SingleBlog></SingleBlog>
             </div>
             <div className=' mx-3 md:mx-0'>
