d1ea7759d8e2df02519438ab13d0c67f61b9c79c
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-01-02 12:40:39 +0600

    linked single session page

--- src/app/(main-layout)/morfitter-pts/[id]/page.js
+++ src/app/(main-layout)/morfitter-pts/[id]/page.js
@@ -40,7 +40,7 @@ const SinglePtSession = () => {
                 </div>
 
                 <div className=' mt-10 grid grid-cols-2 lg:grid-cols-4 gap-10'>
-                    <Link href={`/morfitter-pts/single-session-of-pt/777`}>
+                    <Link href={`/morfitter-sessions/single-session-of-pt/777`}>
                         <div className=' cursor-pointer'>
                             <Image className=' w-full' src={session1} alt='session' width={0} height={0} />
                         </div>
