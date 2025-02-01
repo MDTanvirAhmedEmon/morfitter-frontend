1901f40531c9c330c3af3286436cf9e7085d5f63
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-02-01 16:41:47 +0600

    done my content for trainer

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -55,6 +55,6 @@ const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
 export const baseApi = createApi({
     reducerPath: 'baseApi',
     baseQuery: baseQueryWithRefreshToken,
-    tagTypes: ['user'],
+    tagTypes: ['user', 'content'],
     endpoints: () => ({}),
 });
