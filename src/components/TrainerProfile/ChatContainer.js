5dafd0f0fecb125dca11b74ff9d1347a6927cc78
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-03-06 14:32:32 +0600

    done chats

--- src/components/TrainerProfile/ChatContainer.js
+++ src/components/TrainerProfile/ChatContainer.js
@@ -6,8 +6,11 @@ import { useSelector } from 'react-redux';
 import io from 'socket.io-client';
 
 export default function ChatContainer({ selectedUser, setSelectedUser, messageEndRef }) {
+    const socket = io('http://localhost:5000/live-chats');
     const { user, role } = useSelector((state) => state.auth)
     const { data: messageFromDB } = useGetMessageWithOthersQuery({ sender: user?._id, receiver: selectedUser?.traineeTrainerId });
+    const [message, setMessage] = useState([]);
+    const [messageInput, setMessageInput] = useState('');
 
     const chatContainerRef = useRef(null);  // Reference for the chat container
 
@@ -16,10 +19,34 @@ export default function ChatContainer({ selectedUser, setSelectedUser, messageEn
         if (chatContainerRef.current) {
             chatContainerRef.current.scrollTop = 0;  // Scroll to top
         }
+        if (messageFromDB) {
+            setMessage(messageFromDB?.data);
+        }
     }, [messageFromDB]);  // Trigger when new messages are fetched
 
     const goBackToUserList = () => {
         setSelectedUser(null);
+    };
+    useEffect(() => {
+        socket.on(`received${user?._id}`, (data) => {
+            console.log('from socket', data);
+            setMessage((prevsetMessage) => [...prevsetMessage, data]);
+        });
+        // return () => {
+        //     socket.disconnect();
+        // }
+    }, [user?._id, socket]);
+
+    const sendMessage = () => {
+        console.log(messageInput);
+        if (selectedUser?.traineeTrainerId && message) {
+            socket.emit('sendUser', {
+                sender: user?._id,
+                receiver: selectedUser?.traineeTrainerId,
+                message: messageInput
+            });
+        }
+        setMessageInput('')
     };
 
     return (
@@ -42,7 +69,7 @@ export default function ChatContainer({ selectedUser, setSelectedUser, messageEn
                 ref={chatContainerRef}  // Set ref here
                 className="flex flex-col bg-transparent md:bg-slate-50 p-3 rounded-md mb-2 absolute top-16 bottom-20 right-0 left-0 overflow-auto"
             >
-                {messageFromDB?.data?.map((msg, index) => (
+                {message?.map((msg, index) => (
                     <div key={index} className={`mt-2 flex ${msg?.sender === user?._id ? 'justify-end' : 'justify-start'}`}>
                         <span
                             className={`mb-2 p-2 max-w-xs ${msg?.sender === user?._id
@@ -80,11 +107,10 @@ export default function ChatContainer({ selectedUser, setSelectedUser, messageEn
                     >
                         <Input
                             placeholder="Type message here"
-                            // value={messageInput}
-                            // onChange={(e) => setMessageInput(e.target.value)}
-                        // onPressEnter={handleSendMessage}
+                        value={messageInput}
+                        onChange={(e) => setMessageInput(e.target.value)}
                         />
-                        <button  className=" bg-primary px-4 text-white py-2 rounded-md" >
+                        <button onClick={sendMessage} className=" bg-primary px-4 text-white py-2 rounded-md" >
                             Send
                         </button>
                     </ConfigProvider>
